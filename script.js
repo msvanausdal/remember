8f65c22b51c03d9c71202e383a47afd3a557c5b0
Matthew Van Ausdal <msvanaus@purdue.edu>
2024-04-15 08:22:54 -0400

    update

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
 const interval = 50;
-var num;
-var blurb;
-var encrypted;
-var running;
+const placeholders = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's',
+    'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+let num;
+let blurb;
+let encrypted;
+let running;
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function() {
+    blurb = "";
     running = false;
 });
 
-document.getElementById("remember").addEventListener("click", async function (e) {
+document.getElementById("remember").addEventListener("click", async function () {
     if(!running) {
         running = true;
+        await backspace(blurb);
         num = getNumber();
         blurb = getBlurb(num);
         encrypted = encrypt(blurb);
         await type(encrypted);
-        await visualize();
+        await visualize(blurb, encrypted);
         running = false;
     }
-    //type(encrypted).then(visualize).then(function() {document.getElementById("remember").innerHTML = "hello";});
-    //document.getElementById("blurb").innerHTML = encrypted;
 });
-
-async function type(text) {
-    document.getElementById("blurb").innerHTML = "";
-    var i = 0
-    for(i; i < text.length; i++) {
-        document.getElementById("blurb").innerHTML += text[i];
-        await waitForMs(interval/10);
-    }
-}
 
 function waitForMs(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
@@ -44,11 +38,8 @@ function getBlurb(num) {
 }
 
 function encrypt(original) {
-    var placeholders = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's',
-        'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
     original = original.split('');
-    for(var i = 0; i < original.length; i++){
+    for(let i = 0; i < original.length; i++){
         if (placeholders.includes(original[i])){
             original[i] = placeholders[Math.floor(Math.random() * placeholders.length)];
         }
@@ -57,37 +48,48 @@ function encrypt(original) {
 }
 
 function decrypt(original, encrypted){
-    var placeholders = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's',
-        'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
-
     original = original.split('');
     encrypted = encrypted.split('');
 
-    for(var i = 0; i < original.length; i++){
-        if(encrypted[i] != original[i]){
+    for(let i = 0; i < original.length; i++){
+        if(encrypted[i] !== original[i]){
             encrypted[i] = placeholders[Math.floor(Math.random() * placeholders.length)];
         }
     }
     return encrypted.join('');
 }
 
-function visualize() {
-    var timer = setInterval(
-        function() {
-            encrypted = decrypt(blurb, encrypted);
-            document.getElementById("blurb").innerHTML = encrypted;
 
-            if(encrypted == blurbs){
-                clearInterval(timer);
-            }
-        }, interval);
+
+async function type(text) {
+    document.getElementById("blurb").innerHTML = "";
+    for(let i = 0; i < text.length; i++) {
+        document.getElementById("blurb").innerHTML += text[i];
+        await waitForMs(interval/10);
+    }
 }
 
-var blurbs = [
-    "lorem ipsum dolor sit amet, consectetur adipiscing elit. fusce placerat nibh est, dapibus consequat eros blandit pellentesque. sed id purus quam. curabitur sollicitudin ligula augue, ut euismod leo iaculis vitae. sed a est ex. morbi et ipsum sapien. mauris faucibus dapibus urna, vel tristique diam congue et. ut porttitor lobortis purus sit amet eleifend. nam a ex ut lorem congue tempor at ut nulla. donec vitae justo sed neque scelerisque scelerisque. pellentesque quis justo quis ipsum facilisis malesuada eleifend id lectus. nulla pretium arcu velit, id tempus leo mollis et.",
-    "nunc dignissim odio leo, in lacinia lacus tincidunt vel. fusce vel enim luctus, tristique quam ac, pretium sapien. ut quam lectus, ultricies eu neque id, dapibus tincidunt metus. aenean gravida nec odio ut sollicitudin. interdum et malesuada fames ac ante ipsum primis in faucibus. cras suscipit molestie ex, ac dictum enim rhoncus sed. nam eget condimentum mi. aenean at tristique metus. mauris aliquam mattis commodo. vivamus finibus mauris quam, in mollis lorem finibus in.",
-    "vestibulum a consequat velit, id rutrum diam. etiam augue nunc, vehicula a elit in, vehicula finibus velit. sed tortor tortor, facilisis sed posuere id, sodales a orci. etiam varius vel dui sit amet pellentesque. integer mauris risus, posuere eget dapibus vitae, efficitur et velit. aenean ullamcorper tempus lobortis. vestibulum quis vehicula nunc. curabitur a quam maximus, semper justo sit amet, suscipit ligula. mauris elit nibh, ultrices ut condimentum vel, aliquet iaculis libero. nam tempor tortor et mattis ornare. mauris sit amet nisl eu libero pharetra faucibus. pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. sed vitae est et sem venenatis mollis.",
-    "nunc et felis semper, semper ex id, vehicula lectus. ut euismod rhoncus elit in ultricies. pellentesque sed erat maximus, euismod ipsum rutrum, pharetra libero. in viverra mollis risus, et consequat massa tempor ac. integer non consectetur sapien, in ullamcorper nibh. vestibulum non sagittis massa, nec cursus neque. mauris consequat non tellus non dictum. duis sed augue faucibus, rutrum justo tincidunt, ullamcorper mauris. nam vel libero interdum libero porttitor aliquet vitae et tellus. cras sed venenatis neque. morbi porttitor pretium libero, id dictum ante ultricies in.",
-    "ut faucibus tellus tempor neque egestas, vel molestie est elementum. morbi tempus magna ut lorem mattis lobortis. aliquam ut ligula in lectus porta volutpat. nulla facilisi. nam vel dignissim dui. proin vestibulum, orci sit amet tristique pellentesque, massa eros tempor augue, a maximus enim magna at massa. duis fermentum aliquet faucibus."
+async function backspace(text) {
+    for(let i = text.length - 1; i >= 0; i--) {
+        text = text.slice(0, i);
+        document.getElementById("blurb").innerHTML = text;
+        await waitForMs(interval/10);
+    }
+}
+
+async function visualize(original, text) {
+    while(original !== text) {
+        text = decrypt(original, text);
+        document.getElementById("blurb").innerHTML = text;
+        await waitForMs(interval);
+    }
+}
+
+const blurbs = [
+    "this is text number one, wow you are lucky you got the first one epic.",
+    "this is the second text. it is a bit longer than the first text, but it's still not the first text, you are unlucky.",
+    "this one is definitely also epic, even though its number three. how fun is that, very cool, i know.",
+    "i dont really know what to type at this point i just wanted the words to be legible so that you can really tell when the text is encrypted and not encrypted. this one is super epic long very cool very epic super cool.",
+    "wow this is the last one, number five. that's probably omega unlucky fr. i am completely out of ideas on what to type so now i am just typing.",
+    "jokes on you there was a sixth text, i forgot that i haven't yet tested it with this many. super cool. this one also has an apostrophe earlier, which probably wont be encrypted at all."
 ]
