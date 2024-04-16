ec610af63a1a734c4cca267f88284de41e648f2d
msvanausdal <msvanaus@purdue.edu>
2024-04-16 19:20:54 -0400

    added functionality for characters that dont stop changing

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const placeholdersAll = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's',
     'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
 const placeholdersDate = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const question = ['?'];
 let numArray = [];
 let num;
 let date = "";
@@ -114,7 +115,7 @@ function waitForMs(ms) {
 function encrypt(original, allowedChars) {
     original = original.split('');
     for(let i = 0; i < original.length; i++){
-        if (allowedChars.includes(original[i])){
+        if (allowedChars.includes(original[i]) || question.includes(original[i])){
             original[i] = allowedChars[Math.floor(Math.random() * allowedChars.length)];
         }
     }
@@ -203,9 +204,12 @@ async function backspace(text, elementId, interval = INTERVAL) {
  * @returns {Promise<void>}
  */
 async function visualize(original, text, allowedChars, elementId, interval = INTERVAL) {
-    while(original !== text) {
+    const counter = 50;
+    let i = 0;
+    while(original !== text || i < counter) {
         text = decrypt(original, text, allowedChars);
         document.getElementById(elementId).innerHTML = text;
+        i++;
         await waitForMs(interval);
     }
 }
